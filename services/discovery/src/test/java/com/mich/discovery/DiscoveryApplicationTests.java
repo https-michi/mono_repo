f65033e7096bdcfb5bf62eccd737bde7055a5636
https-michi <glitchmichi@gmail.com>
2025-12-31 00:58:41 -0500

    feat(discovery): initialize discovery service

---
+++ services/discovery/src/test/java/com/mich/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mich.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

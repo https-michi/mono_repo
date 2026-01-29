e30c353bd30ed2fb64bae0a7f754158f1d50d800
https-michi <glitchmichi@gmail.com>
2026-01-29 10:43:25 -0500

    feat(product): introduce product-category domain model and Flyway V1 schema

---
+++ services/product-service/src/main/java/com/mich/ecommerce/product/entity/Category.java
@@ -0,0 +1,27 @@
+package com.mich.ecommerce.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @SequenceGenerator(
+            name = "category_seq",
+            sequenceName = "category_seq",
+            allocationSize = 50
+    )
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
---
+++ services/product-service/src/main/java/com/mich/ecommerce/product/entity/Product.java
@@ -0,0 +1,30 @@
+package com.mich.ecommerce.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(
+            name = "product_seq",
+            sequenceName = "product_seq",
+            allocationSize = 50
+    )
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}

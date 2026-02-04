e444bfe81817c928740e9fac60e495e4fcdfcb74
https-michi <glitchmichi@gmail.com>
2026-02-03 19:01:00 -0500

    feat(order): add feign client for product purchase

---
+++ services/order-service/src/main/java/com/mich/ecommerce/product/dto/PurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.mich.ecommerce.product.dto;
+
+import java.math.BigDecimal;
+
+public record PurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
---
+++ services/order-service/src/main/java/com/mich/ecommerce/product/service/ProductClient.java
@@ -0,0 +1,16 @@
+package com.mich.ecommerce.product.service;
+
+
+import com.mich.ecommerce.product.dto.PurchaseRequest;
+import com.mich.ecommerce.product.dto.PurchaseResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "product-service", url = "${application.config.product-url}")
+public interface ProductClient {
+    @PostMapping("/purchase")
+    List<PurchaseResponse> purchaseProducts(@RequestBody List<PurchaseRequest> requestBody);
+}

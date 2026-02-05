637a8b263607d316c3f2274532259bd47f6172e1
https-michi <glitchmichi@gmail.com>
2026-02-04 20:32:12 -0500

    feat(notification): add order and payment event models and notification persistence

---
+++ services/notification-service/src/main/java/com/mich/ecommerce/notification/entity/Notification.java
@@ -0,0 +1,25 @@
+package com.mich.ecommerce.notification.entity;
+
+import com.mich.ecommerce.notification.enums.NotificationType;
+import com.mich.ecommerce.kafka.order.OrderConfirmation;
+import com.mich.ecommerce.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document
+public class Notification {
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
---
+++ services/notification-service/src/main/java/com/mich/ecommerce/notification/enums/NotificationType.java
@@ -0,0 +1,6 @@
+package com.mich.ecommerce.notification.enums;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
